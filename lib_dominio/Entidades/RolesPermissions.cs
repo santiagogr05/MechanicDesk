8e3acc5dca261162ac079ee8bf94ffdba6999ad2
SamAl404 <samualvarezh@gmail.com>
2025-05-29 14:46:47 -0500

    agrego modelos para relaciones de M:N

---
+++ lib_dominio/Entidades/RolesPermissions.cs
@@ -0,0 +1,12 @@
+﻿
+namespace lib_dominio.Entidades
+{
+    public class RolesPermissions
+    {
+        public int Id { get; set; }
+        public int RoleId { get; set; }
+        public Roles? _Roles { get; set; }
+        public int PermissionId { get; set; }
+        public Permissions? _Permissions { get; set; }
+    }
+}
---
+++ lib_dominio/Entidades/UsersRoles.cs
@@ -0,0 +1,11 @@
+﻿namespace lib_dominio.Entidades
+{
+    public class UsersRoles
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public Users? _Users { get; set; }
+        public int RoleId { get; set; }
+        public Roles? _Roles { get; set; }
+    }
+}
