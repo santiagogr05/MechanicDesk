ccf0046ff09918ff7c38e38a2a7ca5d8952eeaf2
SamAl404 <samualvarezh@gmail.com>
2025-05-29 16:35:52 -0500

    modificar con modelos con data annotations

--- lib_dominio/Entidades/RolesPermissions.cs
+++ lib_dominio/Entidades/RolesPermissions.cs
@@ -1,12 +1,14 @@
 ﻿
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace lib_dominio.Entidades
 {
     public class RolesPermissions
     {
         public int Id { get; set; }
         public int RoleId { get; set; }
-        public Roles? _Roles { get; set; }
+        [ForeignKey("RoleId")] public Roles? _Roles { get; set; }
         public int PermissionId { get; set; }
-        public Permissions? _Permissions { get; set; }
+        [ForeignKey("PermissionId")]public Permissions? _Permissions { get; set; }
     }
 }
--- lib_dominio/Entidades/UsersRoles.cs
+++ lib_dominio/Entidades/UsersRoles.cs
@@ -1,11 +1,13 @@
-﻿namespace lib_dominio.Entidades
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace lib_dominio.Entidades
 {
     public class UsersRoles
     {
         public int Id { get; set; }
         public int UserId { get; set; }
-        public Users? _Users { get; set; }
+        [ForeignKey("UserId")] public Users? _Users { get; set; }
         public int RoleId { get; set; }
-        public Roles? _Roles { get; set; }
+        [ForeignKey("RoleId")] public Roles? _Roles { get; set; }
     }
 }
